e380c3b7c199d606b89ef8d2ad17a3d34cf73772
Nikita Khromov <Nikita.Khromov@onlyoffice.com>
2021-08-03 17:40:58 +0300

    Changed speech service.

--- scripts/speech.js
+++ scripts/speech.js
@@ -18,9 +18,13 @@
 (function(window, undefined)
 {
 	var text_init = "";
-	var is_end_callback = false;
+    var synth     = window.speechSynthesis;
+    var lang_name = "Google UK English Male";
+    var pitch     = 1;
+    var rate      = 1;
+    var voices    = [];
 
-	window.Asc.plugin.init      = function(text)
+	window.Asc.plugin.init = function(text)
 	{
 		if ("" == text)
 		{
@@ -29,103 +33,62 @@
 		}
 
 		text_init = text;
-		function StartCallback()
-		{
-			setTimeout(function(){
-				if (!is_end_callback && !responsiveVoice.isPlaying())
-				{
-					responsiveVoice.speak(text_init, undefined, {onstart : StartCallback, onend : EndCallback, onerror : EndCallback});
-				}
-			}, 5000);
-		}
-
-		function EndCallback()
-		{
-			is_end_callback = true;
-			window.Asc.plugin.button(-1);
-		}
-
-		function Run(lang)
-		{
-			var voicelist = responsiveVoice.getVoices();
-
-			var _data  = [];
-			var _langs = responsiveVoice.responsivevoices;
-
-			var _map   = {};
-			_map["en"] = ["gb"];
-			_map["ko"] = ["kr"];
-			_map["hy"] = ["ar"];
-			_map["uk"] = ["ru"]; //ua not supported
-			_map["zh"] = ["cn"];
-			_map["ja"] = ["jp"];
-			_map["pt-BR"] = ["br"];
-			_map["pt-Pt"] = ["pt"];
-			_map["zh-TW"] = ["tw"];
-			_map["cs"] = ["cz"];
-			_map["da"] = ["dk"];
-			_map["et"] = ["ee"];
-			_map["el"] = ["gr"];
-			_map["va"] = ["la"];
-			_map["ne"] = ["np"];
-			_map["nn"] = ["no"];
-			_map["sl"] = ["sk"];
-			_map["ta"] = ["hi"];
-			_map["ro"] = ["md"];
-			_map["sh"] = ["hr"];
-			_map["ca"] = ["catalonia"];
-			for (var i = 0; i < _langs.length; i++)
-			{
-				if (_langs[i].flag == lang)
-				{
-					_data.push({index : i, gender : _langs[i].gender});
-				}
-				else if (_map[lang])
-				{
-					for (var k = 0; k < _map[lang].length; k++)
-					{
-						if (_langs[i].flag == _map[lang][k])
-						{
-							_data.push({index : i, gender : _langs[i].gender});
-							break;
-						}
-					}
-				}
-			}
-			if (!_data.length) {
-				_data.push({index : 0, gender : "f"});
-			}
-			_data.sort(function(a, b) { return a.gender.charCodeAt(0) - b.gender.charCodeAt(0) }); // family :)
-			var voiceName = "";
-			for (var j = 0; j < _data.length; j++)
-			{
-				var nameCandidate = _langs[_data[j].index].name;
-				for (var k = 0, k_len = voicelist.length; k < k_len; k++)
-				{
-					if (voicelist[k].name === nameCandidate)
-					{
-						voiceName = nameCandidate;
-						break;
-					}
-				}
-				if (voiceName !== "")
-					break;
-			}
-
-			responsiveVoice.speak(text_init, voiceName, {onstart : StartCallback, onend : EndCallback, onerror : EndCallback});
-		}
-
-		responsiveVoice.AddEventListener("OnReady", function() {
-			setTimeout(function()
-			{
-				guessLanguage.info(text_init, function(info) {
-					//console.log('Detected Language: ' + info[2] + " [" + info[0] + "]" +"_____" +info[1]);
-					Run(info[0]);
-				});
-			}, 1);
-		});
+		speak(text_init);
+		window.Asc.plugin.executeCommand("close", "");
 	};
 
+	function speak(inputTxt){
+        if (synth.speaking) {
+            console.error('speechSynthesis.speaking');
+            return;
+        }
+        var utterThis = new SpeechSynthesisUtterance(inputTxt);
+        utterThis.onend = function (event) {
+            console.log('SpeechSynthesisUtterance.onend');
+        }
+        utterThis.onerror = function (event) {
+            console.error('SpeechSynthesisUtterance.onerror');
+        }
+
+        for(i = 0; i < voices.length ; i++) {
+            if(voices[i].name === lang_name) {
+                utterThis.voice = voices[i];
+                break;
+            }
+        }
+
+        utterThis.pitch = pitch;
+        utterThis.rate = rate;
+        synth.speak(utterThis);
+      };
+
+    $(document).ready(function () {
+        function populateVoiceList() {
+            voices = synth.getVoices().sort(function (a, b) {
+                const aname = a.name.toUpperCase(), bname = b.name.toUpperCase();
+                if ( aname < bname ) return -1;
+                else if ( aname == bname ) return 0;
+                else return +1;
+            });
+        };
+
+        populateVoiceList();
+        if (speechSynthesis.onvoiceschanged !== undefined) {
+            speechSynthesis.onvoiceschanged = populateVoiceList;
+        }
+
+        var saved_pitch = localStorage.getItem("plugin-speech-pitch");
+        if (saved_pitch)
+            pitch = saved_pitch;
+
+        var saved_rate = localStorage.getItem("plugin-speech-rate");
+        if (saved_rate)
+            rate = saved_rate;
+
+        var saved_lang = localStorage.getItem("plugin-speech-lang-name");
+        if (saved_lang)
+            lang_name = saved_lang;
+    });
 	window.Asc.plugin.button = function(id)
 	{
 		if (-1 == id)
